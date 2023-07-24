733779d29848facc9fa415aa42923512a7121714
Abdlmu'tii <92731667+abdlmutii@users.noreply.github.com>
2023-07-24 15:51:30 +0300

    Update blogs.js

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const {Blogs} = require('../../schema/blogs.js');
 
 module.exports = {
-  name: '/blogs',
+  name: '/',
   run: async(req, res) => {
     let blogs = await Blogs.find({});
     res.render("../templetes/blogs/blogs.ejs", { blogs: blogs.reverse(), h: blogs[0] });
